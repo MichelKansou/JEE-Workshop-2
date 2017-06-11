4c35771e857382392ece981fe97db3bc1cc1f052
Michel Kansou <michel.kansou@outlook.fr>
2017-06-11 18:25:33 +0200

    add Get payments

--- bankFacade-war/src/main/java/com/bank/paymentmgmt/facade/PaymentResource.java
+++ bankFacade-war/src/main/java/com/bank/paymentmgmt/facade/PaymentResource.java
@@ -5,20 +5,20 @@
  */
 package com.bank.paymentmgmt.facade;
 
+import com.bank.paymentmgmt.facade.domain.Payment;
 import java.io.StringReader;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -61,6 +61,19 @@ public class PaymentResource {
         } else {
             return Response.status(400).entity("n CB invalide").build();
         }
+    }
+    
+    @Path("payments")
+    @GET
+    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    public Response getStoredPayments(){
+        //récupération de tous les ordres de paiement stockés
+        List<Payment> storedPayments = bankingService.lookupAllStoredPayments();
+        //création d'une entité générique pour pouvoir mapper un type paramétré (List<Payment>) avec //un corps de réponse
+        GenericEntity<List<Payment>> genericList = new GenericEntity<List<Payment>>(storedPayments){};
+        //construction de la réponse embarquant dans son corps les ordres de paiements
+        Response resp = Response.ok(genericList).build();
+        return resp;
     }
 
 }
